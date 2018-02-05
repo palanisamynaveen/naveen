c65c5cfaae60151c75692fbb579ec29cebb3094a
palanisamynaveen <35765119+palanisamynaveen@users.noreply.github.com>
2018-02-05 15:51:32 +0530

    naveen2.c

---
+++ naveen2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+{
+int base,exponent;
+long long result =1;
+clrscr();
+printf("enter the base number");
+scanf("%d",&base);
+printf("enter the exponent");
+scanf("%d",&exponent);
+while(exponent ! =0)
+printf("the result is %d",result);
+getch();
+return 0;
+}
