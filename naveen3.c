240f71aa9fc2423571ec33d84706a0d58b5deb8f
palanisamynaveen <35765119+palanisamynaveen@users.noreply.github.com>
2018-02-05 16:12:17 +0530

    naveen3.c

---
+++ naveen3.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+{
+int n,num,rev=o;
+clrscr();
+printf("enter the number to check the palindrome");
+scanf("%d",&n);
+num=n;
+while(n !=0)
+{
+rev =(rev*10)+(n*10)
+}
+if(rev==num)
+{
+printf("the entered number is a palindrome");
+}
+else
+{
+printf("the entered numberis not a palindrome");
+{
+getch();
+return 0;
+}
